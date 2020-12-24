b38ed3c47ca5e6949b66db01365a0691b19ac6f0
The man <ThisIsntTheWay@users.noreply.github.com>
2020-12-24 19:14:34 +0100

    Fix Get-BinanceWalletInfo

--- modules/APIinterface.ps1
+++ modules/APIinterface.ps1
@@ -1,3 +1,7 @@
+################################
+### PS Binance API interface ###
+################################
+
 # ---------------------------------
 # FUNCTIONS
 # ---------------------------------
@@ -58,8 +62,60 @@ function Get-TradingPair {
             $response = "Pair not found"
         }
     }
-
     return $response
+}
+
+# Acquire HMAC SHA256 signature
+function Compute-Signature($message) {
+    # Retrieve APISecret from DB
+    # Afterwards, convert FROM securestring
+    $q = "SELECT APIsecret FROM binanceSettings;"
+    [string]$in = (Construct-Query $q $sqlDB).APISecret
+
+    $secSecret = $in | convertto-securestring
+    $secret = (New-Object PSCredential "user",$secSecret).GetNetworkCredential().Password
+    
+    # Perform HMAC conversion
+    $hmacsha = New-Object System.Security.Cryptography.HMACSHA256
+    $hmacsha.key = [Text.Encoding]::ASCII.GetBytes($secret)
+    $signature = $hmacsha.ComputeHash([Text.Encoding]::ASCII.GetBytes($message))
+    $signature = [System.BitConverter]::ToString($signature) -replace '-', ''
+
+    # Clear secret
+    Remove-Variable secSecret
+    Remove-Variable secret
+    
+    return $signature
+}
+
+function Get-BinanceWalletInfo() {
+    # Acquire Unix Time
+    #[string]$unixTime = ([int][double]::Parse((Get-Date -UFormat %s)))*1000
+    [string]$unixTime = [Math]::Floor([decimal](Get-Date(Get-Date).ToUniversalTime()-uformat "%s")) * 1000
+    $basePoint = "${APIBase}/sapi/v1/capital/config/getall?"
+    $message = "recvWindow=20000&timestamp=${unixTime}"
+
+    $signature = Compute-Signature($message)
+
+    write-host "Signature is: " -fore magenta -NoNewline
+        write-host $signature
+
+    # Extract APIkey from DB and convert to readable format
+    $q = "SELECT APIkey FROM binanceSettings;"
+    [string]$in = (Construct-Query $q $sqlDB).APIKey
+
+    $secKey = $in | convertto-securestring
+    $key = (New-Object PSCredential "user",$secKey).GetNetworkCredential().Password
+
+    # Form request
+    $out = Invoke-WebRequest "${basePoint}${message}&signature=${signature}" `
+        -Headers @{'Content-Type' = 'application/json'; 'X-MBX-APIKEY' = "$key"}
+
+    # Clear key
+    Remove-Variable key
+    Remove-Variable secKey
+
+    return $out
 }
 
 # Parse Binance API response code
