60079e009ad21a3b68a31a50290f175e3acdf515
The man <ThisIsntTheWay@users.noreply.github.com>
2020-12-26 00:04:06 +0100

    Update autoTrade.ps1

--- autoTrade.ps1
+++ autoTrade.ps1
@@ -24,7 +24,7 @@
 [bool]$logVerbosity = $true
 
 # User-defined maximum for $quoteAsset
-[int]$quoteAssetLimit = 100
+[int]$quoteAssetLimit = 0
 
 # The following params are applicable:
 # CONTINUE = Ignore limit violation
@@ -60,7 +60,7 @@ function log {
                 Write-Host "[$(Get-Date -Format 'HH:mm:ss')] ($logSymbol) $in" -fore yellow -back Black
             }
             "X" {
-                Write-Host "[$(Get-Date -Format 'HH:mm:ss')] ($logSymbol) $in" -fore red -back Red
+                Write-Host "[$(Get-Date -Format 'HH:mm:ss')] ($logSymbol) $in" -fore red -back Black
             }
         }
     }
@@ -128,6 +128,10 @@ log "> Found $($limitOrders.count) LIMIT order(s)."
     #  > Cycle through ALL JSONs and somehow merge them all together
 if ($marketOrders.count -ge 1) {
     $global:order = (get-content $marketOrders.FullName) | convertfrom-json
+    if (!($?)) {
+        log "Importing order has failed." "X"
+        exit
+    }
 
     # Workaround for acquiring order count 
     [int]$count = 0
@@ -141,33 +145,83 @@ if ($marketOrders.count -ge 1) {
     [int]$count = 0
     foreach ($a in $order.orders) {
         $count++
-        log "Processing order #${count}..."
-        log "> Target asset: $($a.$count.targetAsset)"
-        log "> Quote asset: $($a.$count.quoteAsset)"
-        log "> Side: $($a.$count.side)"
-        log "> Side mode: $($a.$count.sideMode)"
-        log "> Quantity: $($a.$count.quantity)"
-        log "> Scheduled for: $($a.$count.schedule)"
+        log "BEING rocessing order #${count}..."
+        log "> Target asset: $($a.targetAsset)"
+        log "> Quote asset: $($a.quoteAsset)"
+        log "> Side: $($a.side)"
+        log "> Side mode: $($a.sideMode)"
+        log "> Quantity: $($a.quantity)"
+        log "> Scheduled for: $($a.schedule)"
 
-        log "Attempting to send order to Binance..." ">"
-
-        # Send order
-        if ($a.$count.sideMode -like "QUOTE") {
-            $global:r = Set-BinanceSpotOrder -symbol $a.$count.targetAsset `
-                                             -quoteSymbol $a.$count.quoteAsset `
-                                             -side $a.$count.side `
-                                             -type $order.Type `
-                                             -quoteQty $a.$count.quantity
-        } else {
-            $global:r = Set-BinanceSpotOrder -symbol $a.$count.targetAsset `
-                                             -quoteSymbol $a.$count.quoteAsset `
-                                             -side $a.$count.side `
-                                             -type $order.Type `
-                                             -Qty $a.$count.quantity
+        # Verify that $a.quantity does not exceed $quoteAssetLimit
+        $skip = $false
+        if ($a.sideMode -like "QUOTE") {
+            if ($a.quantity -gt $quoteAssetLimit) {
+                    log "This order exceeds a quote asset limit of '${quoteAssetLimit}'." "!"
+                switch ($quoteAssetLimitViolation) {
+                    "CONTINUE" {
+                        log "> Continuing anyway..." "i"
+                    } "OVERRIDE" {
+                        log "> Quote asset quantity ($($a.quantity)) has been lowered to the limit size (${quoteAssetLimit})." "!"
+                        $quantity = $quoteAssetLimit
+                    } "ABORT" {
+                        log "> This order will be discarded." "X"
+                        $skip = $true
+                    }
+                }
+            } else {
+                $a.quantity = $quoteAssetLimit
+            }
         }
 
-        log "Full HTTP answer from API call:" ">"
-        Write-Output $r >> $logFile
+        # Abort order execution if $skip has been set to true
+        if (!($skip)) {
+            log "Attempting to send order to Binance..." ">"
+    
+            # Send order
+            $nocpmplete = $false
+            if ($a.sideMode -like "QUOTE") {
+                try {
+                    $global:r = Set-BinanceSpotOrder -symbol $a.targetAsset `
+                                                     -quoteSymbol $a.quoteAsset `
+                                                     -side $a.side `
+                                                     -type $order.Type `
+                                                     -quoteQty $a.quantity  `
+                                                     -ErrorAction SilentlyContinue
+                } catch {
+                    $nocomplete = $true
+                }                                                 
+            } elseIf ($a.sideMode -like "ASSET") {
+                try {
+                    $global:r = Set-BinanceSpotOrder -symbol $a.targetAsset `
+                                                     -quoteSymbol $a.quoteAsset `
+                                                     -side $a.side `
+                                                     -type $order.Type `
+                                                     -Qty $a.quantity `
+                                                     -ErrorAction SilentlyContinue
+                                                     write-host $error[0].errordetails.message
+                } catch {
+                    $nocomplete = $true
+                }    
+            } else {
+                $nocomplete = $true
+                log "> Unkown order side: $($a.sideMode)" "X"
+                log "  Order was not dispatched." "X"
+            }
+                
+            if ($nocomplete) {
+                # Check if the last command was successful, and if it was from invoke-webrequest
+                #if ($error[0].InvocationInfo.MyCommand.Name -like "Invoke-WebRequest") {}
+                $err = ($error[0].errordetails.message | convertfrom-json)
+
+                log "Order was not executed by Binance." "X"
+                log "API response: '$($err.code) - $($err.msg)'" "X"
+            } else {
+                log "Order sent to binance successfully."
+                Write-Output $r >> $logFile
+            }
+            log "END processing order #${count}."
+        }
     }
 }
 if ($limitOrders.count -ge 1) {
